fbadc3d00f90d50d76934bf5023754f80fa7932a
kalygreim11@gmail.com <kalygreim11@gmail.com>
2022-10-31 16:00:24 -0300

    tratando erros com Controller Advice

---
+++ src/main/java/com/github/KalyanaGreim/rest/controller/ApplicationControllerAdvice.java
@@ -0,0 +1,20 @@
+package com.github.KalyanaGreim.rest.controller;
+
+import com.github.KalyanaGreim.exception.RegraNegocioException;
+import com.github.KalyanaGreim.rest.ApiErrors;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+public class ApplicationControllerAdvice {
+
+    @ExceptionHandler(RegraNegocioException.class)
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public ApiErrors handleRegraNegocioException(RegraNegocioException exception){
+        String mensagemErro = exception.getMessage();
+        return new ApiErrors(mensagemErro);
+    }
+}
