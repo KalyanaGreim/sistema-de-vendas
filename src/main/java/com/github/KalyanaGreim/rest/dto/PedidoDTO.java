04296aa0d81dd959576ccf554019a2703e234628
kalygreim11@gmail.com <kalygreim11@gmail.com>
2022-10-31 11:16:21 -0300

    Implementando lombok

--- src/main/java/com/github/KalyanaGreim/rest/dto/PedidoDTO.java
+++ src/main/java/com/github/KalyanaGreim/rest/dto/PedidoDTO.java
@@ -1,8 +1,15 @@
 package com.github.KalyanaGreim.rest.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
 import java.math.BigDecimal;
 import java.util.List;
 
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class PedidoDTO {
     private Integer cliente;
     private BigDecimal total;
